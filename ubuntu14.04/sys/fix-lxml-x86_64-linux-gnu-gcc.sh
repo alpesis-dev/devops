54c666fb7424d47d5fa9dd30389dfab86c363e3a
KellyChan <kwailamchan@hotmail.com>
2015-09-30 19:04:31 +0800

    updated the packages

--- ubuntu14.04/sys/fix-lxml-x86_64-linux-gnu-gcc.sh
+++ ubuntu14.04/sys/fix-lxml-x86_64-linux-gnu-gcc.sh
@@ -2,7 +2,7 @@
 
 # fixed x86_64-linux-gnu-gcc error when install lxml
 
-sudo apt-get install gcc
-sudo apt-get install python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi
-
-
+sudo apt-get install libxml2-dev libxslt1-dev python-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install libevent-dev
+sudo pip install lxml
