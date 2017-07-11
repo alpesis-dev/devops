9fbddb808f447205670165ee05bc165653591752
Kelly <kwailamchan@hotmail.com>
2017-07-11 11:27:12 +0800

    [master] added boost 1.57 installation in apps. 	new file:   boost.sh

---
+++ scripts/ubuntu14.04/apps/boost.sh
@@ -0,0 +1,9 @@
+# install boost 1.57
+
+sudo apt-get install build-essential g++ python-dev autotools-dev libicu-dev build-essential libbz2-dev
+cd /tmp
+wget http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2
+tar xvjf ./boost_1_57_0.tar.bz2
+./bootstrap.sh --prefix=/opt/boost_1_57_0
+sudo ./b2
+sudo ./b2 install
