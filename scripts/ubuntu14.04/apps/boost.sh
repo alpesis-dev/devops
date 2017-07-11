2b32c7c0c4ebb9176a170d9dcc8af3e51129ba70
Kelly <kwailamchan@hotmail.com>
2017-07-11 17:46:32 +0800

    [master] updated the dependencies in apps/boost.sh. 	modified:   apps/boost.sh

--- scripts/ubuntu14.04/apps/boost.sh
+++ scripts/ubuntu14.04/apps/boost.sh
@@ -1,6 +1,6 @@
 # install boost 1.57
 
-sudo apt-get install build-essential g++ python-dev autotools-dev libicu-dev build-essential libbz2-dev
+sudo apt-get install build-essential g++ python-dev autotools-dev libicu-dev build-essential libbz2-dev libboost-all-dev
 cd /tmp
 wget http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2
 tar xvjf ./boost_1_57_0.tar.bz2
